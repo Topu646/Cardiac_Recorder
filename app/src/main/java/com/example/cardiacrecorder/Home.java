98a0ec744e3d42e7564484d00d6cdfc85d42e2b5
Proloy Karmakar <proloy@gmail.com>
2023-06-08 21:24:54 +0600

    done with home page

---
+++ app/src/main/java/com/example/cardiacrecorder/Home.java
@@ -0,0 +1,28 @@
+package com.example.cardiacrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+
+    private Button insertdatabutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        insertdatabutton = findViewById(R.id.insertbutton);
+
+        insertdatabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this,Add_data.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
